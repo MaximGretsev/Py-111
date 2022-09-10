192c7322817c086cb0381861c8bad7992c0c4123
MaximGretsev <maximgretsev@gmail.com>
2022-09-10 12:58:27 +0300

    init Taylor.

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,19 @@
 Taylor series
 """
 from typing import Union
+import math
+from itertools import count
+
+# sin(x) -> ((-1) ** (n-1) * x ** (2n - 1)) / (2n - 1)!
+EPSILON = 0.0001
+
+
+def get_item_n(x, n):
+    return x ** n / math.factorial(n)
+
+
+def get_sin(x, n):
+    return ((-1) ** n) * (x ** (2 * n + 1)) / math.factorial((2 * n) + 1)
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +24,13 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    sum_ = 1
+    for n in count(1, 1):
+        current_item = get_item_n(x, n)
+        sum_ += current_item
+        if current_item <= EPSILON:
+            break
+    return sum_
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +40,12 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    sum_ = 0
+    for n in count(0, 1):
+        current_item = get_sin(x, n)
+        print(current_item)
+        sum_ += current_item
+
+        if abs(current_item) <= EPSILON:
+            break
+    return sum_
