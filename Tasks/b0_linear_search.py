0b80a741c28adb144b94bd3b56fc0bf849afe9a0
MaximGretsev <maximgretsev@gmail.com>
2022-09-10 10:34:13 +0300

    init my_linear_search

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -11,5 +11,6 @@ def min_search(arr: Sequence) -> int:
     :param arr: Array containing numbers
     :return: index of first occurrence of minimal element in array
     """
-    print(arr)
-    return -1
+    list_num = list(enumerate(arr, 0))
+    min_ = min(list_num, key=lambda i: i[1])
+    return min_[0]
