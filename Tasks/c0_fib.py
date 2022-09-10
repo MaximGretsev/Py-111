118ce2f50cce842800799245030973d3d9b2092f
MaximGretsev <maximgretsev@gmail.com>
2022-09-10 13:19:37 +0300

    init fibbonachi

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,14 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError("n < 0")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
 def fib_iterative(n: int) -> int:
@@ -17,4 +23,14 @@ def fib_iterative(n: int) -> int:
     :return: Fibonacci number
     """
     print(n)
-    return 0
+    if n < 0:
+        raise ValueError("n < 0")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a = 0
+    b = 1
+    for _ in range(1, n):
+        a, b = b, a+b
+    return b
