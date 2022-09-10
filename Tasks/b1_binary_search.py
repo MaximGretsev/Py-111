f2cb0fb6431a66002a75b257f9d3f07ca728442d
MaximGretsev <maximgretsev@gmail.com>
2022-09-10 12:11:47 +0300

    init binary_search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,15 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    # TODO: Пересмотреть алгоритм
+    right_border = len(arr) - 1
+    left_border = 0
+    while right_border >= left_border:
+        mid_index = left_border + (right_border - left_border) // 2
+        mid_value = arr[mid_index]
+        if mid_value == elem:
+            return mid_index
+        elif elem < mid_value:
+            right_border = mid_index - 1
+        elif elem > mid_value:
+            left_border = mid_index + 1
