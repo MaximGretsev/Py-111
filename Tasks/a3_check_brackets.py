d3cabdfba3da9e6333fde04a3457255c21343b51
Maxim Gretsev <m.gretcev@loyaltyplant.com>
2022-09-12 14:53:41 +0300

    init add TODO

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,23 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    # TODO: Фейлятся тесты на невалидность, но при это работают тесты на корректность. Надо как-то проверить
+    # TODO: что первый символ является открывающей скобочкой, а второй - закрывающей.
+    valid_start = []
+    valid_end = []
+    list_of_brackets = list(brackets_row)
+    for i in list_of_brackets:
+        if i == "(":
+            valid_start.append(i)
+        elif i == ")":
+            valid_end.append(i)
+    print(valid_start)
+    print(valid_end)
+    if len(valid_start) == len(valid_end):
+        return True
+    else:
+        return False
 
-    return False
+
+check_brackets('()()()')
+check_brackets("")
